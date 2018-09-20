306b31ab85a5abf6d0d8373619e69371a6da38ee
Hamidur Rahman <39712889+HamidurRahman1@users.noreply.github.com>
2018-09-20 11:38:24 -0400

    Update Tester.java
    A note for user has been added

--- Console Projects/companyManager/Tester.java
+++ Console Projects/companyManager/Tester.java
@@ -1,3 +1,14 @@
+
+/*
+*
+*   NOTE: When an user creates Employee objects, they must enter unique ID for each employee.
+*         ID range distinguish between different employees.
+*
+*               ID #: 100 <= Manager <= 999;
+*               ID #: 1000 <= Salaried Employee <= 4999;
+*               ID #: 5000 <= Hourly Employee <= 9999;
+* */
+
 package companyManager;
 
 import java.util.List;
